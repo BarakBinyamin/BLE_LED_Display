4b5f781af16efa06bf25ffdd2ed5199e332aa51e
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-03-20 13:39:55 -0400

    LED Display GUI

--- GUI/LedDisplay.py
+++ GUI/LedDisplay.py
@@ -26,10 +26,26 @@ class MainWindow(wx.Frame):
 		button.Bind(wx.EVT_BUTTON, lambda evt, temp="nope": self.OnPlay(evt, temp) )'''
 		x=70
 		y=40
+		self.Off_Buttons = []
+		self.On_Buttons = []
 
+		def Make_button(index):
+			image = wx.Image("LEDoff.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+			button = wx.BitmapButton(self, id=x, bitmap=image, size=(50, 50), pos=(x, y))
+			temp_var = str(index) + "off"
+			button.Bind(wx.EVT_BUTTON, lambda evt, temp=temp_var: self.OnClick(evt, temp) )
+			image2 = wx.Image("LEDon.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+			button2 = wx.BitmapButton(self, id=x, bitmap=image2, size=(50, 50), pos=(x, y))
+			temp_var = str(index) + "on"
+			button2.Bind(wx.EVT_BUTTON, lambda evt, temp=temp_var: self.OnClick(evt, temp) )
+			self.Off_Buttons.append(button)
+			self.On_Buttons.append(button2)
+			button2.Hide()
+	
 		for n in range(5):
-			button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
-			button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			#button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
+			#button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			Make_button(n)
 			print(n)
 
 			dy = 1	 # calculate for next loop
@@ -39,9 +55,11 @@ class MainWindow(wx.Frame):
 		y+=180
 		x=70
 		for n in range(5, 10):
-			button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
-			button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			#button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
+			#button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			Make_button(n)
 			print(n)
+			
 
 			dy = 1	 # calculate for next loop
 			y += dy
@@ -50,8 +68,9 @@ class MainWindow(wx.Frame):
 		y+=180
 		x=70
 		for n in range(10, 15):
-			button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
-			button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			#button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
+			#button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			Make_button(n)
 			print(n)
 
 			dy = 1	 # calculate for next loop
@@ -61,8 +80,9 @@ class MainWindow(wx.Frame):
 		y+=180
 		x=70
 		for n in range(15, 20):
-			button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
-			button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			#button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
+			#button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			Make_button(n)
 			print(n)
 
 			dy = 1	 # calculate for next loop
@@ -72,8 +92,9 @@ class MainWindow(wx.Frame):
 		y+=180
 		x=70
 		for n in range(20, 25):
-			button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
-			button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			#button = wx.Button(self, id=x,  size=(50, 50), pos=(x, y))
+			#button.Bind(wx.EVT_BUTTON, lambda evt, temp=n: self.OnClick(evt, temp) )
+			Make_button(n)
 			print(n)
 
 			dy = 1	 # calculate for next loop
@@ -83,13 +104,32 @@ class MainWindow(wx.Frame):
 
 		self.Show(True)
 
+
 	def OnClick(self, Event, additionalArgument):
-		RowColumn = str(additionalArgument)
+		# ---
+		def switch(string):
+			switch_var = string
+			if "off" in switch_var:
+				switch_var = switch_var.replace('off','')
+				switch_var = int(switch_var)
+				self.button1Click(Event, switch_var)
+				return switch_var
+			if "on" in additionalArgument:
+				switch_var = switch_var.replace('on','')
+				switch_var = int(switch_var)
+				self.button2Click(Event, switch_var)
+				return switch_var
+			return 0
+		# ---
+		arg = switch(additionalArgument)
+		RowColumn = str(arg)
 		print(RowColumn)
 		command = "./write2.sh " + RowColumn
 		tempArray.append(RowColumn)
 		staticArray.append(RowColumn)
 		os.system(command)
+
+
 	def OnSave(self, Event, additionalArgument):
 		RowColumn = str(additionalArgument)
 		print(RowColumn)
@@ -102,8 +142,17 @@ class MainWindow(wx.Frame):
 		print(RowColumn)
 		os.system(command)
 
+	def button1Click(self,event, index):
+		self.Off_Buttons[index].Hide()
+		self.SetTitle("Button1 clicked")
+		self.On_Buttons[index].Show()
+	
+	def button2Click(self,event, index):
+		self.On_Buttons[index].Hide()
+		self.SetTitle("Button2 clicked")
+		self.Off_Buttons[index].Show()
+
+
 app = wx.App(False)
 frame = MainWindow(None, "MAIN")
 app.MainLoop()
-
-
