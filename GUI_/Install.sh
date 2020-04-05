526692d95214fa9d9e4b14c8304a25faa37b8680
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-04-05 13:54:27 -0400

    Add files via upload

---
+++ GUI_/Install.sh
@@ -0,0 +1,18 @@
+path="/home/${USER}"
+
+cd 
+
+pip install pyinstaller
+
+
+sed -i "s#Path#${path}#g" ${path}/BLE_LED_Display/GUI_/LED_GUI.spec
+
+pyinstaller ${path}/BLE_LED_Display/GUI_/LED_GUI.spec
+
+sed -i "s#path#${path}#g" ${path}/BLE_LED_Display/GUI_/Excecutable_Link.desktop
+
+mv ${path}/BLE_LED_Display/GUI_/Excecutable_Link.desktop Desktop
+
+cd Desktop
+
+chmod +x Excecutable_Link.desktop
